6b59630e2132a29e4904b095edab0094bd96131e
briannayamanaka <briannayamanaka@gmail.com>
2016-02-05 06:56:54 +0000

    edited add.js and index.js

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,18 @@
+
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
-	// Your code goes here
+	var name = req.query.name;
+	var description = req.query.name;
+
+	data.friends.push({
+	name: name,
+	description: description,
+	imageURL: 'http://lorempixel.com/400/400'
+	})
+
+	res.render('add',{
+	name: name,
+	description: description
+			})
  }
